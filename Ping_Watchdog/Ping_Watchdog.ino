22382e14d4448d92f928107ecbcea33a911fbc6d
Gustavo de León <alfonso.gus.deleon@gmail.com>
2020-05-15 18:46:00 -0500

    Refactorizado y cambiado el metodo de obtener la IP del servidor al que se le hará Ping
    En el ultimo commit, el metodo de obtener la ip era manual, y ademas se necesitaba escribir la IP de manera extraña, para acomodarse al modo de que la libreria ESP8266Ping.h trabaja, ahora solo es necesario escribir un nombre de host o una IP normalmente en una variable y el programa hace el resto, aún si la ip de el host cambia entre cada ping.

--- Ping_Watchdog/Ping_Watchdog.ino
+++ Ping_Watchdog/Ping_Watchdog.ino
@@ -6,17 +6,19 @@
 #include <ESP8266Ping.h>
 #include <ESP8266HTTPClient.h>
 
-#define IFTTT_host  "maker.ifttt.com"
+#define ssid "SSID"
+#define password "password"
+
+char host_remoto[] = "Nombre_De_Host_Remoto_O_IP";
+IPAddress ip_remota; //IP a monitorear
+bool ult_estado = false;
+
+#define IFTTT_host "maker.ifttt.com"
 #define IFTTT_Maker_Key "Tu_key" //Añade tus credenciales de IFTTT, Puedes encontrar tu key en https://ifttt.com/maker_webhooks/settings
 #define IFTTT_Event "Tu_Evento" //Añade el nombre de tu evento, para esto tendras que haber creado primero un Applet en IFTTT con el Trigger Webhooks
 String value1;
 String value2;
 String value3;
-
-#define ssid  "SSID"
-#define password  "password"
-
-IPAddress ip_remota(192, 168, 1, 1); //IP a monitorear, cambiar por ip por defecto remplazando puntos por comas de la nueva IP
 
 WiFiClient client;
 HTTPClient http;
@@ -25,7 +27,7 @@ void setup() {
   Serial.begin(250000);
   delay(10);
 
-  // Nos Conectamos a una Red WiFi
+  //Nos Conectamos a una Red WiFi
   Serial.println("\nConectando a WiFi");
 
   WiFi.begin(ssid, password);
@@ -41,25 +43,38 @@ void setup() {
 }
 
 void loop() {
-  if ((WiFi.status() == WL_CONNECTED)) {
-
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFi.hostByName(host_remoto, ip_remota); //Resolviendo el host a monitorear a IP
 
     Serial.print("Haciendo ping a: ");
+    Serial.print(host_remoto);
+    Serial.print(" con ip: ");
     Serial.println(ip_remota);
 
-    if (Ping.ping(ip_remota)) {
+    if (Ping.ping(ip_remota) && ult_estado == false) {
       Serial.println("¡¡EXITO!!");
       http.begin(client, "http://" IFTTT_host "/trigger/" IFTTT_Event "/with/key/" IFTTT_Maker_Key "?value1=" + value1 + "&value2=" + value2 + "&value3=" + value3); //HTTP
       int httpCode = http.GET();
       Serial.println(httpCode);
+      if (httpCode == 200) {
+        Serial.println("Contactado IFTTT con exito");
+      } else {
+        Serial.println("Error al contactar con IFTTT :( No internet?");
+      }
       Serial.println(http.getString());
-    } else {
+      ult_estado = true;
+    } else if (Ping.ping(ip_remota) == false && ult_estado == true) {
       Serial.println("Error :(");
       http.begin(client, "http://" IFTTT_host "/trigger/" IFTTT_Event "/with/key/" IFTTT_Maker_Key "?value1=" + value1 + "&value2=" + value2 + "&value3=" + value3); //HTTP
       int httpCode = http.GET();
       Serial.println(httpCode);
+      if (httpCode == 200) {
+        Serial.println("Contactado IFTTT con exito");
+      } else {
+        Serial.println("Error al contactar con IFTTT :( No internet?");
+      }
       Serial.println(http.getString());
-
+      ult_estado = false;
     }
     delay(3000);
   }
