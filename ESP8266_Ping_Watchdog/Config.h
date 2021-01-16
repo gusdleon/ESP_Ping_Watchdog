9f401bf9aa76d6c89317cac4e85c38d2375f6450
Gustavo de León <alfonso.gus.deleon@gmail.com>
2021-01-15 19:01:58 -0600

    Actualizada huella digital SSL de IFTTT
    Se actualizo la huella digital de IFTTT a la ultima version, nueva fecha de expiración: 29/05/2021

--- ESP8266_Ping_Watchdog/Config.h
+++ ESP8266_Ping_Watchdog/Config.h
@@ -11,7 +11,7 @@ byte n_pings = 1; //numero de pings a realizar
 #define IFTTT_Maker_Key "TU_KEY" //Añade tus credenciales de IFTTT, Puedes encontrar tu key en https://ifttt.com/maker_webhooks/settings
 #define IFTTT_Event "TU_EVENTO" //Añade el nombre de tu evento, para esto tendras que haber creado primero un Applet en IFTTT con el Trigger Webhooks
 
-// Huella digital de *.ifttt.com expira el 28/09/2020 pero es posible que deje de funcionar antes.
-const uint8_t huellaDigital[20] = {0xaa, 0x75, 0xcb, 0x41, 0x2e, 0xd5, 0xf9, 0x97, 0xff, 0x5d, 0xa0, 0x8b, 0x7d, 0xac, 0x12, 0x21, 0x08, 0x4b, 0x00, 0x8c};
+// Huella digital de *.ifttt.com expira el 29/05/2021 pero es posible que deje de funcionar antes.
+const uint8_t huellaDigital[20] = {0x24, 0x8E, 0x25, 0x32, 0x85, 0xE5, 0xE8, 0x64, 0x2C, 0xC9, 0x94, 0x41, 0x1A, 0xB2, 0xFB, 0x84, 0x36, 0x2B, 0xDD, 0x09};
 
 unsigned long intervalo = 30000;
