1aff8251e333c6cf0dc0047ad0804f4e5bca8665
Gustavo de León <alfonso.gus.deleon@gmail.com>
2021-06-18 16:21:59 -0500

    Actualizada huella digital de IFTTT

--- ESP8266_Ping_Watchdog/Config.h
+++ ESP8266_Ping_Watchdog/Config.h
@@ -11,7 +11,7 @@ byte n_pings = 1; //numero de pings a realizar
 #define IFTTT_Maker_Key "TU_KEY" //Añade tus credenciales de IFTTT, Puedes encontrar tu key en https://ifttt.com/maker_webhooks/settings
 #define IFTTT_Event "TU_EVENTO" //Añade el nombre de tu evento, para esto tendras que haber creado primero un Applet en IFTTT con el Trigger Webhooks
 
-// Huella digital de *.ifttt.com expira el 29/05/2021 pero es posible que deje de funcionar antes.
-const uint8_t huellaDigital[20] = {0x24, 0x8E, 0x25, 0x32, 0x85, 0xE5, 0xE8, 0x64, 0x2C, 0xC9, 0x94, 0x41, 0x1A, 0xB2, 0xFB, 0x84, 0x36, 0x2B, 0xDD, 0x09};
+// Huella digital de *.ifttt.com expira el 29/04/2022 pero es posible que deje de funcionar antes.
+const uint8_t huellaDigital[20] = {0xB8, 0x71, 0x16, 0x7C, 0x62, 0x9F, 0x5E, 0xAA, 0x33, 0x46, 0x83, 0x2A, 0x7E, 0x95, 0xAD, 0x4B, 0xE6, 0xB2, 0xB8, 0x07};
 
 unsigned long intervalo = 30000;
